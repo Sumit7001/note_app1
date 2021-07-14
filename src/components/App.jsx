a555b592535fc5707dddb5ddc08c22ff9b3821c6
Souvik Goon <souvikgoon22@gmail.com>
2021-07-14 14:01:26 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  const [notesList, setNotesList] = useState([]);
+
+  function addNote(newNote) {
+    setNotesList((prev) => {
+      return [...prev, newNote];
+    });
+  }
+
+  function deleteNote(idOfNote) {
+    setNotesList((prev) => {
+      return prev.filter((val, index) => {
+        return index !== idOfNote;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notesList.map((note, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={note.title}
+            content={note.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
